f7169c50255965ba1689a31023a468d575609745
Rafaela Ribeiro <rafaelaribeiro1103@gmail.com>
2025-12-17 12:15:07 -0300

    adicionando desafio boas-vindas

---
+++ desafio-boas-vindas/script.js
@@ -0,0 +1,5 @@
+function verificar(botao){
+    const nome = prompt('Digite seu nome:')
+    const idade = prompt(`Olá ${nome}, digite sua idade:`)
+    const res = alert (`Acabei de conhecer ${nome}, que tem ${idade} anos`)
+}
