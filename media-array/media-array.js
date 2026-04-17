43bd8b96afcec5fa4b50a657063b144f9a462a0f
Rafaela Ribeiro <rafaelaribeiro1103@gmail.com>
2026-04-17 14:25:20 -0300

    enunciado-exercicio

---
+++ media-array/media-array.js
@@ -0,0 +1,10 @@
+//Escreva uma função que retorne a média de todos os números de um array
+// Se o array estiver vazio, retorne 0
+
+function mediaArray(numeros) {
+  // faça aqui
+}
+
+console.log(mediaArray([2, 4, 6])) // 4
+console.log(mediaArray([10, 20])) // 15
+console.log(mediaArray([])) // 0
